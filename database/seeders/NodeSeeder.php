4bc5299eb99928379e1a8a4318e067f4b2b5e0c9
Ruslana <kuzina.ruslana@yandex.ru>
2025-04-15 17:44:58 +0300

    Make seeders for nodes

---
+++ database/seeders/NodeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Node;
+
+class NodeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $level1 = Node::create(['title' => 'Уровень 1']);
+        $child11 = Node::create(['title' => 'Потомок 1.1', 'parent_id' => $level1->id]);
+        $child12 = Node::create(['title' => 'Потомок 1.2', 'parent_id' => $level1->id]);
+        $child121 = Node::create(['title' => 'Потомок 1.2.1', 'parent_id' => $child12->id]);
+
+        $level2 = Node::create(['title' => 'Уровень 2']);
+        $child21 = Node::create(['title' => 'Потомок 2.1', 'parent_id' => $level2->id]);
+    }
+}
