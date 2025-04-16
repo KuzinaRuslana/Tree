06c975984a69d7989ae8f778d046c8861611daa2
Ruslana <kuzina.ruslana@yandex.ru>
2025-04-16 19:11:25 +0300

    Add a fillable property to the Node Model

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Node extends Model
 {
+
+    protected $fillable = ['title', 'parent_id'];
+
     public function children()
     {
         return $this->hasMany(Node::class, 'parent_id');
